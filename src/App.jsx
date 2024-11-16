b99969ac9705ef88f3cba21cec4ca9c64e59b1a4
Misherz <michelle.j.lugo@gmail.com>
2024-11-15 21:27:24 -0500

    Added Header

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,16 @@ import './App.css'
 import { getData } from './data/data.mjs'
 
 function App() {
+  const today = new Date()
+  const day = today.getDate()
+  const month = today.getMonth()
+  const year= today.getFullYear();
 
+  
 
   return (
     <>
-      <h1>Test</h1>
+      <h1>Top News of {day}/{month}/{year}</h1>
     </>
   )
 }
